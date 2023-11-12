22a08b8b2b1fb698712e911047890aeb2d9252e2
Peter Macdonald <macdonald.peter90@gmail.com>
2023-11-12 17:23:14 +0100

    init repo, moved to its own plugin
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

---
+++ src/api/types.ts
@@ -0,0 +1,21 @@
+import { Entity } from "@backstage/catalog-model";
+import { createApiRef } from "@backstage/core-plugin-api";
+
+export interface OpaBackendApi {
+  entityCheck(entityMetadata: Entity): Promise<OpaResult>;
+}
+
+export const opaBackendApiRef = createApiRef<OpaBackendApi>({
+  id: "plugin.opa-backend.service",
+});
+
+export interface OpaResult {
+  allow: boolean;
+  violation?: Violation[];
+}
+
+export interface Violation {
+  id: number;
+  level: "error" | "warning";
+  message: string;
+}
